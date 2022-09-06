a42b744c81902ded6a435d06e69e53702f5c5b38
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-06 14:54:55 -0300

    Styling the list with css file

--- src/components/coursesList/courseList.js
+++ src/components/coursesList/courseList.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import courses from '../../Data/courses.json'
+import courses from '../../Data/courses.json';
+import  './courses.css';
 
 const CourseList = () => {
     console.log(courses)
 
-    return <div>{
-        courses.map(course => <div key={course.id}>
+    return <div className='courses'>{
+        courses.map(course => <div key={course.id} className='course'>
             <div className='img_container'> 
             <img className='course_img' src={course.courseImage} alt={course.title} />
             </div>
