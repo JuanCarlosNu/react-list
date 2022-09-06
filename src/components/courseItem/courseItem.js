80f65ae5a315b3021c85b9a4816d5bac6c16c38c
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-06 15:49:30 -0300

    use props to pass values

--- src/components/courseItem/courseItem.js
+++ src/components/courseItem/courseItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const courseItem = () => {
     return ( 
-        <div key={course.id} className='course'>
+        <div  className='course'>
             <div className='img_container'> 
             <img className='course_img' src={course.courseImage} alt={course.title} />
             </div>
--- src/components/coursesList/courseList.js
+++ src/components/coursesList/courseList.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import courses from '../../Data/courses.json';
+import courseItem from '../courseItem/courseItem';
 import  './courses.css';
 
 const CourseList = () => {
-    console.log(courses)
 
-    return <div className='courses'>{
-        courses.map(course => <div key={course.id} className='course'>
-            <div className='img_container'> 
-            <img className='course_img' src={course.courseImage} alt={course.title} />
-            </div>
-            <h3>{course.title}</h3>
-            <span>{course.author}</span>
-            <span>{course.price}</span>
-        </div>)
-        }</div> ;
-}
+    return (<div className='courses'>{
+        courses.map(course => <courseItem key={course.id} course={course} /> )}
+        </div>
+    );
+} ;
  
 export default CourseList;
