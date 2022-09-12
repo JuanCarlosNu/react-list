3649a85f95adb5d13621b3574ebc9eef0a8eb75c
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-12 14:16:43 -0300

    replace any <a> navlinks

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NavBar.css'
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
@@ -9,11 +10,11 @@ const NavBar = () => {
                 <h2 className='logo'>This logo</h2>
                 <nav className='nav'>
                     <ul className='nav__list'>
-                        <li className='nav__item'><a href='#' className='nav__link'>Home</a></li>
-                        <li className='nav__item'><a href='#' className='nav__link'>Courses</a></li>
-                        <li className='nav__item'><a href='#' className='nav__link'>About</a></li>
-                        <li className='nav__item'><a href='#' className='nav__link'>Jobs</a></li>
-                        <li className='nav__item'><a href='#' className='nav__link'>Blog</a></li>
+                        <li className='nav__item'><NavLink to='/home' className='nav__link'>Home</NavLink></li>
+                        <li className='nav__item'><NavLink to='/courses' className='nav__link'>Courses</NavLink></li>
+                        <li className='nav__item'><NavLink to='/about' className='nav__link'>About</NavLink></li>
+                        <li className='nav__item'><NavLink to='/jobs' className='nav__link'>Jobs</NavLink></li>
+                        <li className='nav__item'><NavLink to='/blog' className='nav__link'>Blog</NavLink></li>
                     </ul>
                 </nav>
             </div>
