4f7ac91b65a2ffcad9e9fdbc67d086a8195916ce
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-12 13:45:07 -0300

    more about the exact: redirect if unknown

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React from "react"
 import CourseList from "./CourseList/CourseList.js"
 import NavBar from "./NavBar/NavBar.js"
+import {Redirect, Route, Switch,} from "react-router-dom"
+import About from "./About/About.js"
+import Blog from "./Blog/Blog.js"
+import Jobs from "./Jobs/Job.js"
+
 
 
 const App = () => {
 
     return <div>
         <NavBar />
-        <CourseList />
+        <Switch>
+        <Route path="/home" component={CourseList} />
+        <Route path="/about" exact component={About} />
+        <Route path="/courses" exact component={CourseList} />
+        <Route path="/jobs" exact component={Jobs} />
+        <Route path="/blog" exact component={Blog} />
+        <Redirect from="/" exact to="/home" />
+        <Redirect to="/about" />
+        </Switch>
+                  
          </div>
 }
 
