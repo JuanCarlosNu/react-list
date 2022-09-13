5f7972c202d34c943dd9be2dd52c27c880611d62
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-13 12:47:07 -0300

    14-Add the html for the course details

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import courses from '../../Data/courses.json';
 
-
 const CourseDetails = (props) => {
     const courseID = props.match.params.id;
-    const course = courses.find(c =>c.id === Number(courseID) )
-    console.log(course)
-    
-    return (  
-        <div> Course Details </div>
+    const course = courses.find(c =>c.id === Number(courseID) );
+
+     return (  
+        <div className='course__details'>
+            <div className='image__container--d'>
+                <img 
+                 className='course__img--d'
+                 src={course.courseImage} 
+                 alt={course.title} 
+                 />
+             </div>
+
+            <div className='course__content--d'>
+              <span className={course.isHot ? 'course__status' : ''}>
+                {course.isHot ? "Hot" : null}
+             </span>
+
+                <h3 className='course__title--d'>{course.title}</h3>
+                <p className='course__desc--d'>{course.desc}</p>
+                <span className='course__price--d'>{course.price}</span>
+
+            </div>
+        </div>
     );
-}
+};
  
 export default CourseDetails; 
