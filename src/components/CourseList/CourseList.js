fa8d89bbe0b1b20adfda472ec0e6b99c835936bb
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-15 13:31:22 -0300

    66 05 add input markup

--- src/components/CourseList/CourseList.js
+++ src/components/CourseList/CourseList.js
@@ -4,26 +4,29 @@ import CourseItem from '../CourseItem/CourseItem.js';
 import  './courses.css';
 //class based component:
 class CourseList extends React.Component{
-//constructor function: this holds the mangment of the statements.
+ //constructor function: this holds the mangment of the statements.
     constructor() {
         super()
 
         this.state = {
-            courses: courses
-        }
+            courses
+        };
     }
 
-    render(){
-    if (this.state.courses.length === 0)
-     return <div className='noItems'>No Items Found</div>;
+    render() {
+     return (
+      <div className='itemList'>
 
-    return (
-    <div className='courses'>
-        {this.state.courses.map(course =>
+         <form className='search__form'>
+            <input type='text' placeholder='search course' className='course__search' />
+         </form>
+         <div className='courses'>
+             {this.state.courses.map(course =>(
              <CourseItem key={course.id} course={course} /> 
-            )}
+             ))}
+           </div>
         </div>
-        );
+     );
     }
 }
 
