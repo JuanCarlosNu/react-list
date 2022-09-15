245004e18783b4b3e3a8a2af6aa4ff74914d93a3
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-15 12:04:22 -0300

    64- 03 the constructor function and the state

--- src/components/CourseList/CourseList.js
+++ src/components/CourseList/CourseList.js
@@ -2,20 +2,26 @@ import React from 'react';
 import courses from '../../Data/courses.json';
 import CourseItem from '../CourseItem/CourseItem.js';
 import  './courses.css';
-
 //class based component:
-
 class CourseList extends React.Component{
+//constructor function: this holds the mangment of the statements.
+    constructor() {
+        super()
+
+        this.state = {
+            courses: courses
+        }
+    }
 
     render(){
-    if (courses.length === 0)
+    if (this.state.courses.length === 0)
      return <div className='noItems'>No Items Found</div>;
 
     return (
     <div className='courses'>
-        {courses.map(course =>(
+        {this.state.courses.map(course =>
              <CourseItem key={course.id} course={course} /> 
-            ))}
+            )}
         </div>
         );
     }
@@ -24,10 +30,10 @@ class CourseList extends React.Component{
 
 
 
-const CourseList = () => {
+//const CourseList = () => {
 
     
     
-} ;
+//} ;
  
 export default CourseList;
