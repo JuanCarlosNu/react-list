c727c0d95da9270093a1b678a55bd0f08d206dd2
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-26 18:53:56 -0300

    just doing

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import * as Yup from 'yup'
 
-const signupSchema = Yup.object().shape({
-    username: Yup.string().min(4, 'too short').required('mandatory'),
-    emal: Yup.string().email().required('madatory'),
-    password: Yup.string().min(6, '6 character are requ').required('mandatory')
-})
+
+
 const Signup = () => {
     return ( 
       <div><h1>signup</h1></div>
@@ -13,6 +9,3 @@ const Signup = () => {
 }
  
 export default Signup;
-<div>
-    <h1>Signup</h1>
-</div>
