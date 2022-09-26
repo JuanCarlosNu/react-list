74e3d4127b6d7952ee868004704f30ad6ebe8190
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-26 15:56:35 -0300

    85. 04-our first step in formik

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import About from "./About/About.js"
 import Blog from "./Blog/Blog.js"
 import Jobs from "./Jobs/Job.js"
 import CourseDetails from "./CourseDetails/CourseDetails.js"
+import Signup from "./Signup/Singup.js"
 
 
 
@@ -21,6 +22,7 @@ const App = () => {
         <Route path="/courses" exact component={CourseList} />
         <Route path="/jobs" exact component={Jobs} />
         <Route path="/blog" exact component={Blog} />
+        <Route path="/Signup" exact component={Signup} />
         <Redirect from="/" exact to="/home" />
         <Redirect to="/about" />
         </Switch>
