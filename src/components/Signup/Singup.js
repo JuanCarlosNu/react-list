adf3f21855b1f8844c1c3c126a2e83cf742a7093
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-26 16:03:19 -0300

    86. 05-the Formik Component

--- src/components/Signup/Singup.js
+++ src/components/Signup/Singup.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Formik} from 'formik';
 import * as Yup from 'yup';
 
-const singupShema = Yup.object().shape({
+const singupShema = Yup.object).shape({
     username: Yup.string().min(4, 'too short').required('mandatory'),
     email: Yup.string().email().required('required'),
     password: Yup.string().min(6, 'at least 6 characters').required('mandatory to enter one')
@@ -9,9 +10,10 @@ const singupShema = Yup.object().shape({
 })
 
 const Signup = () => {
-    return (
-        <div>Signup</div>
-      );
-}
+    return 
+       <Formik  initialValues={ {name: "", email:"", password:""}} validationSchema={singupShema}
+       />;
+      
+};
  
 export default Signup;
