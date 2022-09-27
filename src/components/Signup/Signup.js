7d0a48cc7bb107f5ea8ca74979a96b05c048dcce
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-27 14:41:28 -0300

    87. 06-the signup function

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -7,14 +7,23 @@ const SignupSchema = Yup.object().shape({
     username: Yup.string().min(4, 'too short').required('mandatory'),
     email: Yup.string().email().required('mandatory'),
     password: Yup.string().min(6, '6characters').required('must enter one'),
-})
+});
 
 const Signup = () => {
-    return 
+    const signup = () => {};
+    return (
         <Formik
-        initialValues={{name:"" , email: "" , password: "" }} validationSchema={SignupSchema} 
-        />;
-     
+        initialValues={{name:"" , email: "" , password: "" }} 
+        validationSchema={SignupSchema} 
+        onSubmit={Signup}
+        render={() => (
+            <div>
+                <h1>Signup</h1>
+               
+            </div>
+        )}
+        />
+    );
 };
  
 export default Signup;
