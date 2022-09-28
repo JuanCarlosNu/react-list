9466e222500a01adff1683c9031c2652994f4e90
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-28 11:58:19 -0300

    93. 12-create a validation schema

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import * as Yup from 'yup';
+
+const loginValidationSchema = Yup.object().shape({
+    email: Yup.string().email('invalid email').required('email can not be empty'),
+    password: Yup.string().required('password can not be empty').min(6, 'at least 6 characters')
+})
 
 const Login = () => {
     return ( 
